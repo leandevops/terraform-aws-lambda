a2efbcc71401e66269ea3bca9d8e539a58843fad
Andrey Larin <lestex@gmail.com>
2019-03-13 16:08:26 +0300

    adds fixture module

---
+++ test/fixtures/tf_module/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+module "module_test" {
+  source              = "../../../module"
+  lambda_name         = "test_lambda"
+  runtime             = "python3.6"
+  lambda_zipfile      = "${path.module}/function/function.zip"
+  source_code_hash    = "${base64sha256(file("function/function.zip"))}"
+  handler             = "test_lambda.lambda_handler"
+  schedule_expression = "cron(0 04 * * ? *)"
+  policy_document     = "${file("policies/lambda-policy.json")}"
+  timeout             = 60
+
+  project     = "test"
+  description = "Test lambda function for testkitchen run"
+}
