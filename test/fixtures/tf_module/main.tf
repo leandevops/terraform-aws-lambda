8fe508e14153890ee9edf9524c260b2098f41d77
Andrey Larin <lestex@gmail.com>
2019-03-13 17:49:14 +0300

    updates main.tf

--- test/fixtures/tf_module/main.tf
+++ test/fixtures/tf_module/main.tf
@@ -7,10 +7,10 @@ module "module_test" {
   lambda_name         = "test_lambda"
   runtime             = "python3.6"
   lambda_zipfile      = "${path.module}/function/function.zip"
-  source_code_hash    = "${base64sha256(file("function/function.zip"))}"
+  source_code_hash    = "${base64sha256(file("${path.module}/function/function.zip"))}"
   handler             = "test_lambda.lambda_handler"
   schedule_expression = "cron(0 04 * * ? *)"
-  policy_document     = "${file("policies/lambda-policy.json")}"
+  policy_document     = "${file("${path.module}/policies/lambda-policy.json")}"
   timeout             = 60
 
   project     = "test"
